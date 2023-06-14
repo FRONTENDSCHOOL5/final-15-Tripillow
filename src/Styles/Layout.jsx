a734a2b7832df0606033ee9691d72ab2b377ccf1
hmkimm <lisamin7@naver.com>
2023-06-15 00:08:53 +0900

    Design: 글로벌 스타일 height 변경, 최상단 Layout height, border 추가 (#12)

--- src/Styles/Layout.jsx
+++ src/Styles/Layout.jsx
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 const Layout = styled.div`
   margin: 0 auto;
   max-width: 390px;
+  border: 1px solid var(--light-gray);
+  height: 100%;
+
 `;
 
 export default Layout;
