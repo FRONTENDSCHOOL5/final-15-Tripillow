6c6e6d71c22336a2d67d238adbc4d05e1227325b
dasom im <bringvotrevin@gmail.com>
2023-06-16 02:10:27 +0900

    Fix: TopNav를 BasicHeader로 교체

--- src/Components/UploadedPost.jsx
+++ src/Components/UploadedPost.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Comment from '../Comment';
-import PostComment from './PostComment';
-import HomePostLayout from '../HomePost/HomePostLayout';
-import { Layout } from '../../Styles/Layout';
+import { Layout } from '../Styles/Layout';
+import Comment from './Comment';
+import BasicHeader from './common/Header/BasicHeader';
+import PostComment from './common/PostComment';
+import HomePostLayout from './HomePost/HomePostLayout';
 
 export default function UploadedPost() {
   return (
     <Layout>
+      <BasicHeader></BasicHeader>
       <HomePostLayout></HomePostLayout>
       <Comment author={{ username: 'dasom' }} content='댓글임'></Comment>
       <Comment author={{ username: 'dasom' }} content='댓글임'></Comment>
