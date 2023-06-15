a02280a8afee9a9bd60fec3a0c4999d388ffa1f7
haron <haronxlee@gmail.com>
2023-06-15 11:41:15 +0900

    Test: image 테스트를 위한 test폴더 추가

---
+++ src/test/images.js
@@ -0,0 +1,19 @@
+const images = [
+  {
+    id: 0,
+    alt: '그림1',
+    src: 'https://plus.unsplash.com/premium_photo-1682390303849-9cf52609ce82?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+  },
+  {
+    id: 1,
+    alt: '그림2',
+    src: 'https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1721&q=80',
+  },
+  {
+    id: 2,
+    alt: '그림3',
+    src: 'https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+  },
+];
+
+export default images;
