aed0f01917a32b624c6b12fb040f65e4d49ee1f6
dasom im <bringvotrevin@gmail.com>
2023-06-15 16:54:02 +0900

    Design: Comment 텍스트 스타일 변경

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -55,10 +55,12 @@ const ProfileImg = styled.img`
 
 const UserName = styled.span`
   font-size: var('--sm');
+  font-weight: 500;
 `;
 
 const Time = styled.span`
   color: var(--dark-gray);
+  font-size: 10px;
 `;
 
 const MoreBtn = styled.button`
@@ -72,4 +74,5 @@ const MoreBtn = styled.button`
 const Text = styled.p`
   margin: 0 0 0 51px;
   box-shadow: solid 1px 0 0;
+  font-size: var(--sm);
 `;
