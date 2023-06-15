5eec087eb01f715774c0272af32a0d35a4b1c340
dasom im <bringvotrevin@gmail.com>
2023-06-15 17:04:26 +0900

    Rename: UploadedPost 경로 변경

---
+++ src/Components/UploadedPost.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Comment from './Comment';
+import PostComment from './common/PostComment';
+import HomePostLayout from './HomePost/HomePostLayout';
+import { Layout } from '../Styles/Layout';
+
+export default function UploadedPost() {
+  return (
+    <Layout>
+      <HomePostLayout></HomePostLayout>
+      <Comment author={{ username: 'dasom' }} content='댓글임'></Comment>
+      <Comment author={{ username: 'dasom' }} content='댓글임'></Comment>
+      <PostComment></PostComment>
+    </Layout>
+  );
+}
