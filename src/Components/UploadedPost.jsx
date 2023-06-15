b0e4197e6a4bccdcd88b9288fc98ef974bad720e
dasom im <bringvotrevin@gmail.com>
2023-06-16 04:05:07 +0900

    Design: UploadedPost 댓글창 상단 border UI (#36)

--- src/Components/UploadedPost.jsx
+++ src/Components/UploadedPost.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Layout } from '../Styles/Layout';
 import Comment from './Comment';
 import BasicHeader from './common/Header/BasicHeader';
@@ -9,10 +10,17 @@ export default function UploadedPost() {
   return (
     <Layout>
       <BasicHeader></BasicHeader>
-      <HomePostLayout></HomePostLayout>
-      <Comment author={{ username: 'dasom' }} content='댓글임'></Comment>
-      <Comment author={{ username: 'dasom' }} content='댓글임'></Comment>
+      <HomePostLayout isEditable></HomePostLayout>
+      <CommentLayout>
+        <Comment author={{ username: 'dasom' }} content='댓글임'></Comment>
+        <Comment author={{ username: 'dasom' }} content='댓글임'></Comment>
+      </CommentLayout>
       <PostComment></PostComment>
     </Layout>
   );
 }
+
+const CommentLayout = styled.section`
+  border-top: 1px solid var(--light-gray);
+  padding-top: 20px;
+`;
