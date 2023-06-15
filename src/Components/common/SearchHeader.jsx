2b010248d4c3f251b87f3bb2ef67b063886d6e9d
이윤석 <ystone0213@naver.com>
2023-06-15 15:34:00 +0900

    Design: SearchHeader 마크업 & 스타일링 (#12)

---
+++ src/Components/common/SearchHeader.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+import HeaderLayout from '../../Styles/HeaderLayout';
+import prev from '../../Assets/icons/icon-arrow-back.svg';
+import { useNavigate } from 'react-router-dom';
+
+const SearchHeader = () => {
+  const navigate = useNavigate();
+  return (
+    <HeaderLayout>
+      <PrevButton
+        onClick={() => {
+          navigate(-1);
+        }}
+      />
+      <SearchInput />
+    </HeaderLayout>
+  );
+};
+
+const PrevButton = styled.button`
+  width: 22px;
+  height: 22px;
+  margin-right: 8px;
+  background-image: url(${prev});
+`;
+const SearchInput = styled.input`
+  width: 100%;
+  height: 32px;
+  border: none;
+  border-radius: 32px;
+  background-color: #f2f2f2;
+  font-size: var(--sm);
+  padding-left: 16px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default SearchHeader;
