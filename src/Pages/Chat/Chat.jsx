b80b1da4fd30a3dacf407c6bce776ef0efb1be3d
이윤석 <ystone0213@naver.com>
2023-06-16 14:43:14 +0900

    Feat: Chat 페이지 채팅 전송 기능 구현 (#56)

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -1,25 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { LayoutStyle } from '../../Styles/Layout';
 import BasicHeader from '../../Components/common/Header/BasicHeader';
 import profileSm from '../../Assets/profile-sm.png';
 import styled from 'styled-components';
 
 const Chat = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [chatValue, setChatValue] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleButtonClicked = () => {
+    const newChatValue = [...chatValue];
+    newChatValue.push(inputValue);
+    setChatValue(newChatValue);
+    console.log(inputValue);
+    setInputValue('');
+    console.log(inputValue);
+  };
+
   return (
     <ChatLayout>
       <BasicHeader>윤석짱짱123</BasicHeader>
       <ChatContentLayout>
         <UserImage src={profileSm} alt='프로필 사진' />
-        <ChatContent>
+        <ChatContent bgColor='white' radius='0 22px 22px 22px'>
           옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와
           약동하다. 대고, 못할 넣는 풍부하게 뛰노는 인생의 힘있다.
         </ChatContent>
         <ChatTime>12:39</ChatTime>
       </ChatContentLayout>
+      {chatValue.map((_, i) => (
+        <ChatContentLayout marginLeft='auto'>
+          <ChatTime>12:39</ChatTime>
+          <ChatContent color='white' radius='22px 0 22px 22px'>
+            {chatValue[i]}
+          </ChatContent>
+        </ChatContentLayout>
+      ))}
+
       <ChatInputBar>
         <UserImage src={profileSm} alt='프로필 이미지' />
-        <ChatInput placeholder='메시지 입력하기...' />
-        <SendButton>전송</SendButton>
+        <ChatInput onChange={handleInputChange} placeholder='메시지 입력하기...' />
+        <SendButton onClick={handleButtonClicked}>전송</SendButton>
       </ChatInputBar>
     </ChatLayout>
   );
@@ -30,29 +55,33 @@ const ChatLayout = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
+  background-color: #f2f2f2;
+  padding-left: 16px;
+  padding-right: 16px;
 `;
 
 const ChatContentLayout = styled.div`
   display: flex;
   margin-bottom: 10px;
+  margin-left: ${(props) => props.marginLeft};
 `;
 
 const UserImage = styled.img`
   width: 42px;
   height: 42px;
-  padding-left: 16px;
   margin-right: 12px;
 `;
 
 const ChatContent = styled.p`
   max-width: 240px;
   border: 1px solid #c4c4c4;
-  background-color: white;
   padding: 12px;
   box-sizing: border-box;
+  background-color: ${(props) => props.bgColor || 'var(--primary)'};
+  color: ${(props) => props.color};
   font-size: var(--sm);
   line-height: 18px;
-  border-radius: 0 22px 22px 22px;
+  border-radius: ${(props) => props.radius};
 `;
 
 const ChatTime = styled.span`
@@ -93,6 +122,9 @@ const SendButton = styled.button`
   width: 44px;
   height: 44px;
   color: var(--light-gray);
+
+  && {
+  }
 `;
 
 export default Chat;
