60003c4af42e7c27926cfacdc11f924c312477dd
hmkimm <lisamin7@naver.com>
2023-06-17 19:35:57 +0900

    Feat: CircleButton에 페이지 이동 기능 추가

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
-import SnsButton from '../../Components/common/SnsButton';
+import { useNavigate } from 'react-router-dom';
 import BasicHeader from '../../Components/common/Header/BasicHeader';
 import ProductItem from '../../Components/common/ProductItem';
 import Navbar from '../../Components/common/Navbar';
-import Button from '../../Components/common/Button';
 import home from '../../Assets/icons/icon-home.svg';
-import Toggle from '../../Components/common/Toggle';
 import { Layout } from '../../Styles/Layout';
+import CircleButton from '../../Components/common/CircleButton';
+import Toggle from '../../Components/common/Toggle';
 
 export default function Product(props) {
   const navigate = useNavigate();
   return (
-    <Layout>
+    <StyledLayout>
       <BasicHeader>판매 중인 상품</BasicHeader>
+      <Toggle leftButton='외화' rightButton='여행용품' margin='0 0 25px 0' />
       <GridLayout>
         <ProductItem />
         <ProductItem />
         <ProductItem />
-        <ProductItem />
       </GridLayout>
-      <CircleButton bottom='100px'></CircleButton>
+      <CircleButton
+        onClick={() => {
+          navigate('/addproduct');
+        }}
+        right='0'
+        bottom='24px'
+        bgUrl={home}
+      ></CircleButton>
       <Navbar />
-      <button onClick={()=> {navigate('/addproduct')}}>button</button>
-    </Layout>
+    </StyledLayout>
   );
 }
 
+const StyledLayout = styled(Layout)`
+  padding: 48px 12px 73px 16px;
+`;
 const GridLayout = styled.div`
-  padding-left: 19px;
   display: grid;
   grid-gap: 20px;
   grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
