ef48b8b6b2a4ec86f981f86ff90bcda31f4f1d5d
hmkimm <lisamin7@naver.com>
2023-06-19 01:51:56 +0900

    Feat: AddProduct 상품등록 기능 추가, 이미지 업로드 API 추가

--- src/Pages/Product/AddProduct.jsx
+++ src/Pages/Product/AddProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import Toggle from '../../Components/common/Toggle';
 import styled from 'styled-components';
 import Navbar from '../../Components/common/Navbar';
@@ -7,14 +7,17 @@ import Input from '../../Components/common/Input';
 import UploadHeader from '../../Components/common/Header/UploadHeader';
 import URL from '../../Utils/URL';
 import userToken from '../../Recoil/userToken/userToken';
+import ImageUploadAPI from '../../Utils/ImageUploadAPI';
+import defaultImage from '../../Assets/addproduct.png';
 
 const AddProduct = (props) => {
   const [productName, setproductName] = useState('');
   const [price, setPrice] = useState('');
   const [saleLink, setSaleLink] = useState('');
-  const [token, setToken] = useRecoilState(userToken);
+  const [imageLink, setImageLink] = useState('');
+  const token = useRecoilValue(userToken);
+  const imageURL = imageLink
 
-  console.log(token);
   const handleSubmit = async () => {
     // e.preventDefault();
     // e.stopPropagation();
@@ -23,15 +26,16 @@ const AddProduct = (props) => {
       const response = await fetch(URL + '/product', {
         method: 'POST',
         headers: {
-          Authorization: 'Bearer {token}',
+          Authorization: `Bearer ${token}`,
           'Content-type': 'application/json',
         },
+
         body: JSON.stringify({
           product: {
             itemName: productName,
-            price: price, //1원 이상
+            price: parseInt(price), //1원 이상
             link: saleLink,
-            itemImage: String,
+            itemImage: imageLink,
           },
         }),
       });
@@ -41,18 +45,27 @@ const AddProduct = (props) => {
       console.error('에러 발생!!!!!');
     }
   };
+  const handleChange = async (e) => {
+    const response = await ImageUploadAPI(e);
+    setImageLink(`${URL}/${response.filename}`);
+    console.log('@@@@@@@this2!@!#$#@$@#$', imageLink);
+    // console.log(response)
+  };
 
   return (
     <Layout>
-      <UploadHeader disabled onClick={handleSubmit}>
+      <UploadHeader onClick={handleSubmit} disabled={!productName || !price || !saleLink}>
         저장
       </UploadHeader>
-
-      <Image src='https://slp-statics.astockcdn.net/static_assets/staging/22spring/kr/illustrations/categories/card-2.jpg?width=580&format=webp' />
+      <Label htmlFor='file-upload'>
+        <Image src={imageLink || defaultImage} />
+      </Label>
+      <input id='file-upload' className='a11y-hidden' onChange={handleChange} type='file' />
 
       <CategoryTxt>카테고리</CategoryTxt>
       <Toggle margin='0 0 20px 0' leftButton='여행용품' rightButton='외화' />
-      <Input width='100%'
+      <Input
+        width='100%'
         value={productName}
         onChange={(e) => setproductName(e.target.value)}
         label='상품명'
@@ -75,7 +88,7 @@ const AddProduct = (props) => {
         type='url'
         mb='16px'
       />
-      <button onClick={handleSubmit}>btn</button>
+      {/* <button onClick={handleSubmit}>btn</button> */}
       <Navbar />
     </Layout>
   );
@@ -90,12 +103,20 @@ const Layout = styled.div`
   margin: 0 auto;
   border: 1px solid var(--light-gray);
 `;
-const Image = styled.img`
+const Label = styled.label`
+  display: block;
   //fixme: 패딩 값 무시하고 가로 꽉 채우는 다른 방법?
   width: calc(100% + 16px + 12px); // Image 너비에 패딩값 차감
+  height: 232px;
   margin-left: -16px;
   margin-right: -12px;
   margin-bottom: 14px;
+  cursor: pointer;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
 const CategoryTxt = styled.div`
   color: var(--dark-gray);
