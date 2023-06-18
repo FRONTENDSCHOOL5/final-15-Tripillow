bd465cf2996a20f12c5b2a1ef0a31b85b1332118
이윤석 <ystone0213@naver.com>
2023-06-18 17:49:04 +0900

    Search 페이지 검색기능 구현 (#47)

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from '../Styles/Layout';
 import SearchHeader from '../Components/common/Header/SearchHeader';
 import Navbar from '../Components/common/Navbar';
 import User from '../Components/common/User';
 import styled from 'styled-components';
+import URL from '../Utils/URL';
+import userToken from '../Recoil/userToken/userToken';
+import { useRecoilValue } from 'recoil';
 
 const Search = () => {
+  const token = useRecoilValue(userToken);
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [searchData, setSearchData] = useState([]);
+
+  const handleSearchKeyword = (e) => {
+    setSearchKeyword(e.target.value);
+  };
+
+  useEffect(() => {
+    const searchUser = async () => {
+      try {
+        const response = await fetch(`${URL}/user/searchuser/?keyword=${searchKeyword}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = await response.json();
+        setSearchData(data);
+      } catch (error) {
+        console.error('에러', error);
+      }
+    };
+    searchKeyword !== '' && searchUser();
+  }, [searchKeyword, token]);
+
   return (
     <Layout>
-      <SearchHeader />
+      <SearchHeader value={searchKeyword} onChange={handleSearchKeyword} />
       <SearchContentLayout>
-        <User username='애월읍 위니브 감귤농장' content='@weniv_Mandarin' />
+        <ul>
+          {searchData?.map((user) => (
+            <SearchedUser key={user._id}>
+              <User userimage={user.image} username={user.username} content={user.accountname} />
+            </SearchedUser>
+          ))}
+        </ul>
       </SearchContentLayout>
       <Navbar />
     </Layout>
@@ -19,6 +54,10 @@ const Search = () => {
 
 const SearchContentLayout = styled.div`
   padding: 20px 16px;
+`;
+
+const SearchedUser = styled.li`
+  margin-bottom: 16px;
 `;
 
 export default Search;
