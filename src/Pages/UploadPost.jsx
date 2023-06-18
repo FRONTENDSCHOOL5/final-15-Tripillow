530b5c91e5921cd4108d6926a3bb1be5ea2f624a
dasom im <bringvotrevin@gmail.com>
2023-06-18 16:41:03 +0900

    Feat: 이미지 아이콘 클릭시 FormDatat로 포스트 후 첨부파일 표시

--- src/Pages/UploadPost.jsx
+++ src/Pages/UploadPost.jsx
@@ -1,14 +1,16 @@
 import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import { LayoutStyle } from '../Styles/Layout';
-import CircleButton from '../Components/common/CircleButton';
-import imgFile from '../Assets/icons/upload-file.svg';
+import iconImg from '../Assets/icons/upload-file.svg';
 import UploadHeader from '../Components/common/Header/UploadHeader';
 import Toggle from '../Components/common/Toggle';
+import URL from '../Utils/URL';
+import x from '../Assets/icons/x.svg';
 
 export default function UploadPost() {
-  const [inputValue, setInputValue] = useState('');
   const textarea = useRef();
+  const [inputValue, setInputValue] = useState('');
+  const [imgURL, setImgURL] = useState([]);
 
   const handelResizeHeight = () => {
     textarea.current.style.height = 'auto';
@@ -20,6 +22,19 @@ export default function UploadPost() {
     handelResizeHeight();
   };
 
+  const handleImageInput = async (e) => {
+    // TODO 3개 넘을 경우 알림 띄우기
+    if (imgURL.length >= 3) return;
+    const formData = new FormData();
+    formData.append('image', e.target.files[0]);
+    const res = await fetch(URL + '/image/uploadfile', {
+      method: 'POST',
+      body: formData,
+    });
+    const json = await res.json();
+    setImgURL((prev) => prev.concat(`${URL}/${json.filename}`));
+  };
+
   return (
     <PostLayout>
       <UploadHeader disabled={!inputValue}>업로드</UploadHeader>
@@ -27,10 +42,18 @@ export default function UploadPost() {
         <ToggleLayout>
           <ToggleTitle>여행지</ToggleTitle>
           <Toggle leftButton='국내' rightButton='환전' margin='0 0 22px 0'></Toggle>
-          <TextInput placeholder='게시글 입력하기...' ref={textarea} onChange={handelInputChange}></TextInput>
-          {/* <ImgInput></ImgInput> */}
-          <CircleButton bgUrl={imgFile} right='16px' bottom='16px'></CircleButton>
         </ToggleLayout>
+        <TextInput placeholder='게시글 입력하기...' ref={textarea} onChange={handelInputChange} rows='1'></TextInput>
+        {imgURL.map((el, i) => (
+          <ImgLayout key={`ImgLayout-${i}`}>
+            <Img src={el} key={`Img-${i}`} />
+            <ImgDelete key={`ImgDelete-${i}`}></ImgDelete>
+          </ImgLayout>
+        ))}
+        <label htmlFor='img-input'>
+          <ImgIcon src={iconImg}></ImgIcon>
+        </label>
+        <input id='img-input' className='a11y-hidden' type='file' onChange={handleImageInput} />
       </Form>
     </PostLayout>
   );
@@ -43,22 +66,53 @@ const PostLayout = styled.div`
 const ToggleLayout = styled.section`
   margin: 10px 12px 0 16px;
 `;
+
 const ToggleTitle = styled.h1`
   color: var(--dark-gray);
   font-size: var(--xs);
   margin-bottom: 10px;
 `;
+
 const TextInput = styled.textarea`
   border: none;
-  width: 100%;
-  min-height: 30px;
+  width: calc(100% - 28px);
+  margin: 0 12px 20px 16px;
+  /* min-height: 30px; */
   font-size: var(--sm);
   resize: none;
   font: inherit;
+  line-height: 1.2em;
 
   ::placeholder {
     color: var(--gray);
   }
 `;
-// const ImgInput = styled.input``;
+
+const ImgLayout = styled.div`
+  margin: 0 12px 20px 16px;
+  position: relative;
+`;
+
+const Img = styled.img`
+  /* height: 225px; */
+  width: 100%;
+`;
+
+const ImgDelete = styled.section`
+  position: absolute;
+  top: 9px;
+  right: 9px;
+  width: 22px;
+  height: 22px;
+  background: url(${x}) 0 0 / cover;
+`;
+
+const ImgIcon = styled.img`
+  position: absolute;
+  right: 16px;
+  bottom: 16px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
 const Form = styled.form``;
