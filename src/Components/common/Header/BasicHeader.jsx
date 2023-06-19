9343c49d5c98572425d189f1a086f1f3c6fac69b
haron <haronxlee@gmail.com>
2023-06-19 23:20:47 +0900

    Feat: Basic Header의 더보기 버튼에 bottom modal 추가, bottom modal에 AlertModal에 로그아웃 기능추가 (#12)

--- src/Components/common/Header/BasicHeader.jsx
+++ src/Components/common/Header/BasicHeader.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import prev from '../../../Assets/icons/icon-arrow-back.svg';
 import more from '../../../Assets/icons/icon-more-vertical.svg';
 import styled from 'styled-components';
 import HeaderLayout from '../../../Styles/HeaderLayout';
 import { useNavigate } from 'react-router-dom';
+import Modal from '../Modal';
+import { useRecoilState } from 'recoil';
+import userToken from '../../../Recoil/userToken/userToken';
+import isLogin from '../../../Recoil/isLogin/isLogin';
+import accountName from '../../../Recoil/accountName/accountName';
+import AlertModal from '../AlertModal';
 
 const BasicHeader = (props) => {
   const navigate = useNavigate();
+  const [modal, setModal] = useState(false);
+  const [alertModal, setAlertModal] = useState(false);
+  const [token, setToken] = useRecoilState(userToken);
+  const [login, setLogin] = useRecoilState(isLogin);
+  const [name, setName] = useRecoilState(accountName);
+
+  const handleMorebutton = () => {
+    setModal(!modal);
+  };
+
+  const handleLogoutbutton = () => {
+    setAlertModal(!alertModal);
+  };
+
+  const handleCancel = () => {
+    setAlertModal(false);
+    setModal(false);
+  };
+
+  const handleLogout = () => {
+    setToken('');
+    setLogin(false);
+    setName('');
+    navigate('/');
+  };
+
   return (
     <HeaderLayout>
       <ContentLayout>
@@ -17,7 +49,24 @@ const BasicHeader = (props) => {
         />
         {props.children && <div>{props.children}</div>}
       </ContentLayout>
-      <MoreButton />
+      <MoreButton onClick={handleMorebutton} />
+      {modal && (
+        <Modal
+          btn1='설정 및 개인정보'
+          btn2='로그아웃'
+          handleMorebutton={handleMorebutton}
+          handleLogoutbutton={handleLogoutbutton}
+        />
+      )}
+
+      {alertModal && (
+        <AlertModal
+          txt='정말 로그아웃 하시겠습니까?'
+          rightbtn='로그아웃'
+          logout={handleLogout}
+          handleCancel={handleCancel}
+        />
+      )}
     </HeaderLayout>
   );
 };
