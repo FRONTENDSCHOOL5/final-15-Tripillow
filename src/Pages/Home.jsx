a79040033c2bb059c5ba9f38a3bf7074c8ef686e
이윤석 <ystone0213@naver.com>
2023-06-19 11:55:48 +0900

    Feat: Home 피드 API 추가, 그에 따라 피드 뿌려주는 방식 수정 (#16)

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,14 +5,40 @@ import Toggle from '../Components/common/Toggle';
 import HomePost from '../Components/HomePost/HomePostLayout';
 import TopButton from '../Components/common/Topbutton';
 import Navbar from '../Components/common/Navbar';
+import URL from '../Utils/URL';
+import userToken from '../Recoil/userToken/userToken';
+import { useRecoilValue } from 'recoil';
 
 const Home = () => {
-  const [postCount, setPostCount] = useState([...Array(10).fill(1)]);
+  const token = useRecoilValue(userToken);
+
+  const [FeedCount, setFeedCount] = useState(0);
+
+  const [FeedFollowed, setFeedFollowed] = useState([]);
+
+  useEffect(() => {
+    const getFeedFollowed = async () => {
+      try {
+        const response = await fetch(`${URL}/post/feed/?limit=10&skip=${FeedCount}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = await response.json();
+        console.log(data);
+        setFeedFollowed(data.posts);
+      } catch (error) {
+        console.error('에러', error);
+      }
+    };
+    getFeedFollowed();
+  }, [FeedCount, token]);
 
   const handleScroll = () => {
     const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
     if (scrollTop + clientHeight >= scrollHeight) {
-      setPostCount((prevCount) => [...prevCount, ...Array(10).fill(1)]);
+      setFeedCount(FeedCount + 1);
     }
   };
 
@@ -27,6 +53,8 @@ const Home = () => {
     <Layout>
       <MainHeader />
       <Toggle margin='25px 0 0 16px' leftButton='국내' rightButton='해외' />
+      {FeedFollowed.length > 0 && FeedFollowed.map((post) => <HomePost key={post.id} post={post} />)}
+
       <TopButton />
       <Navbar />
     </Layout>
