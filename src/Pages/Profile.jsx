3448c908a99b21f60d103bce90b942374af164fc
haron <haronxlee@gmail.com>
2023-06-20 02:46:06 +0900

    Feat: 프로필페이지 게시물 보기(목록,앨범)추가 (#15)

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
 import UserProfile from '../Components/Profile/UserProfile';
 import BasicHeader from '../Components/common/Header/BasicHeader';
 import HomePostLayout from '../Components/HomePost/HomePostLayout';
@@ -9,8 +10,13 @@ import ProductItem from '../Components/common/ProductItem';
 import UserInfoAPI from '../Utils/UserInfoAPI';
 import GetPostAPI from '../Utils/GetPostAPI';
 import ProductListAPI from '../Utils/ProductListAPI';
-import { useRecoilValue } from 'recoil';
 import accountName from '../Recoil/accountName/accountName';
+import ViewImage from '../Components/HomePost/ViewImage';
+
+import listOn from '../Assets/icons/icon-post-list-on.svg';
+import listOff from '../Assets/icons/icon-post-list-off.svg';
+import AlbumOn from '../Assets/icons/icon-post-album-on.svg';
+import AlbumOff from '../Assets/icons/icon-post-album-off.svg';
 
 const Profile = () => {
   const name = useRecoilValue(accountName);
@@ -18,13 +24,16 @@ const Profile = () => {
   const postData = GetPostAPI();
   const productData = ProductListAPI(name);
   const productList = productData.product;
-  console.log(productList);
+
+  const [view, setView] = useState(false);
+
+  const handleView = () => {
+    setView(!view);
+  };
 
   return (
     <Layout>
-      <header>
-        <BasicHeader />
-      </header>
+      <BasicHeader />
       <main>
         <UserProfile user={userData} />
         <UserProductLayout>
@@ -34,11 +43,28 @@ const Profile = () => {
             <ProductItem />
           </ProductListLayout>
         </UserProductLayout>
+        <ViewLayout>
+          <ViewButton bgImg={!view ? listOn : listOff} onClick={handleView}></ViewButton>
+          <ViewButton bgImg={view ? AlbumOn : AlbumOff} onClick={handleView}></ViewButton>
+        </ViewLayout>
         <article>
-          {postData &&
-            postData.map((post, index) => {
-              return <HomePostLayout key={index} post={post} />;
-            })}
+          {postData.length > 0 ? (
+            <>
+              {!view ? (
+                postData.map((post, index) => <HomePostLayout key={index} post={post} />)
+              ) : (
+                <ImageLayoutBackground>
+                  <ImageLayout>
+                    {postData.map((post, index) => (
+                      <ViewImage key={index} post={post} />
+                    ))}
+                  </ImageLayout>
+                </ImageLayoutBackground>
+              )}
+            </>
+          ) : (
+            <NoContent>게시물이 없습니다.</NoContent>
+          )}
         </article>
       </main>
       <footer>
@@ -51,17 +77,12 @@ const Profile = () => {
 const Layout = styled.div`
   ${LayoutStyle}
   background-color:#f2f2f2;
-
-  main {
-    article {
-      margin-bottom: 6px;
-    }
-  }
 `;
 
 const UserProductLayout = styled.article`
   padding: 20px 16px;
   background-color: #fff;
+  margin: 6px 0;
 
   h2 {
     font-size: var(--md);
@@ -75,10 +96,51 @@ const ProductListLayout = styled.div`
   gap: 10px;
   overflow-x: auto;
   -ms-overflow-style: none;
+  scrollbar-width: none;
 
   ::-webkit-scrollbar {
     display: none;
   }
+`;
+
+const NoContent = styled.p`
+  margin-top: 40px;
+  text-align: center;
+  color: var(--dark-gray);
+`;
+
+const ViewLayout = styled.div`
+  padding: 9px 21px;
+  background-color: #fff;
+  display: flex;
+  justify-content: flex-end;
+
+  button {
+    display: block;
+  }
+
+  button:first-child {
+    margin-right: 13px;
+  }
+`;
+
+const ViewButton = styled.button`
+  width: 26px;
+  height: 26px;
+  background: ${(props) => `url(${props.bgImg}) no-repeat center center`};
+`;
+
+const ImageLayoutBackground = styled.article`
+  min-height: 615px;
+  background-color: #fff;
+`;
+
+const ImageLayout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(114px, 1fr));
+  gap: 8px;
+  padding: 14px 12px 20px 16px;
+  padding-bottom: ${(props) => props.pb || '20px'};
 `;
 
 export default Profile;
