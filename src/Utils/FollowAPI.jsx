594e54b287f4897ab16a3e6154a4db675182458f
haron <haronxlee@gmail.com>
2023-06-22 16:17:21 +0900

    Feat: Follow관련 API 추가 및 수정 (#15)

---
+++ src/Utils/FollowAPI.jsx
@@ -0,0 +1,26 @@
+import { useRecoilValue } from 'recoil';
+import URL from './URL';
+import userToken from '../Recoil/userToken/userToken';
+
+const FollowAPI = ({ account }) => {
+  const token = useRecoilValue(userToken);
+
+  const followUser = async () => {
+    try {
+      const response = await fetch(`${URL}/profile/${account}/follow`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error('FollowAPI 응답이 없습니다.', error);
+    }
+  };
+
+  return { followUser };
+};
+
+export default FollowAPI;
