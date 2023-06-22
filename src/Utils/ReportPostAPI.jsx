1880030302de38878cf7b47d070d3f2bc82a15e5
dasom im <bringvotrevin@gmail.com>
2023-06-22 15:38:54 +0900

    Feat: Delete, Report API

---
+++ src/Utils/ReportPostAPI.jsx
@@ -0,0 +1,27 @@
+import userToken from '../Recoil/userToken/userToken';
+import { useRecoilValue } from 'recoil';
+import URL from './URL';
+
+const ReportPostAPI = (postId, commentId) => {
+  const token = useRecoilValue(userToken);
+
+  const reportPost = async () => {
+    try {
+      const response = await fetch(`${URL}/post/${postId}/report`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('[ERROR on ReportPostAPI]');
+    }
+  };
+
+  return reportPost;
+};
+
+export default ReportPostAPI;
