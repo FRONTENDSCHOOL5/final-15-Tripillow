34d317a259f3609bd3310a30e8ee4b75b673277b
haron <haronxlee@gmail.com>
2023-06-24 16:19:05 +0900

    Feat: Landing페이지에서 미로그인시 타경로 차단 경고창 생성 (#70)

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { LayoutStyle } from '../Styles/Layout';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import character from '../Assets/tripillowmain.svg';
 import LoginMethod from '../Components/LoginMethod';
+import { useLocation } from 'react-router-dom';
+import AlertTop from '../Components/common/AlertTop';
 
 const Landing = () => {
+  const location = useLocation();
+  console.log('🚀  location:', location.state);
+  const [errorMSG, setErrorMSG] = useState();
+
+  useEffect(() => {
+    if (!!location.state) {
+      setErrorMSG(location.state);
+      setTimeout(() => {
+        setErrorMSG(''); // 2초 후에 알림을 없애기 위해 errorMSG 상태를 null로 업데이트
+      }, 2000);
+    }
+  }, []);
+
   return (
     <LandingLayout>
+      {errorMSG && (
+        <AlertTop top='0px' newAnimation isError={!!location.state}>
+          {errorMSG}
+        </AlertTop>
+      )}
       <img src={character} alt='캐릭터' />
       <LoginMethod />
     </LandingLayout>
