39ef917ec14d39c1ac26203238d56d6e90664fc4
dasom im <bringvotrevin@gmail.com>
2023-06-27 10:27:10 +0900

    Feat: AlertTopModal 중첩 문제 해결(#12)

--- src/Components/common/Modal/AlertTop.jsx
+++ src/Components/common/Modal/AlertTop.jsx
@@ -25,8 +25,11 @@ const AlertLayout = styled.div`
     60% {
       transform: translateY(0px);
     }
-    100% {
+    99% {
       transform: translateY(-60px);
+    }
+    100% {
+      transform: translateY(-120px);
     }
   }
 
@@ -38,9 +41,11 @@ const AlertLayout = styled.div`
     60% {
       transform: translateY(0px);
     }
-
-    100% {
+    99% {
       transform: translateY(-70px);
+    }
+    100% {
+      transform: translateY(-140px);
     }
   }
 `;
