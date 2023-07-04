5adf8cbce920749bd8fceafc6da572d2b4a83f4e
haron <haronxlee@gmail.com>
2023-07-04 23:27:43 +0900

    Refactor: 최상위 Layout 스타일 border top과 bottom 제거 (#195)

--- src/Styles/Layout.jsx
+++ src/Styles/Layout.jsx
@@ -6,7 +6,8 @@ const LayoutStyle = css`
   margin: 0 auto;
   box-sizing: border-box;
   padding: 48px 0 73px;
-  border: 1px solid var(--light-gray);
+  border-right: 1px solid var(--light-gray);
+  border-left: 1px solid var(--light-gray);
 `;
 
 const Layout = styled.div`
