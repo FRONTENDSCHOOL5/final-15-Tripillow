150379697583cb52ddd776820f0dc5b071f3cc68
이윤석 <ystone0213@naver.com>
2023-08-10 20:12:34 +0900

    Refactor: 홈페이지 리액트쿼리 사용 및 스크롤 감지 방식 변경 (#243)

--- src/App.js
+++ src/App.js
@@ -24,59 +24,64 @@ import Setting from './Pages/Profile/Setting';
 import { useEffect } from 'react';
 import useIsDesktop from './Components/PCNav/useIsDesktop';
 import PCNavBar from './Components/PCNav/PCNavBar';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function App() {
   const setScreenSize = () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   };
+  const queryClient = new QueryClient();
+
   useEffect(() => {
     setScreenSize();
   });
 
   const isPCScreen = useIsDesktop();
   return (
-    <RecoilRoot>
-      <>
-        <GlobalStyle />
-        {isPCScreen && <PCNavBar />}
-        <Routes>
-          <>
-            <Route path='/' element={<Landing />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route
-              element={
-                <ProtectRoute>
-                  <Outlet />
-                </ProtectRoute>
-              }
-            >
-              <Route path='/home' element={<Home />} />
-              <Route path='/search' element={<Search />} />
-              <Route path='/profile' element={<Profile />} />
-              <Route path='/profile/:accountname' element={<Profile />} />
-              <Route path='/profile/edit' element={<UserProfileSetting />} />
-              <Route path='/profile/followers' element={<Followers />} />
-              <Route path='/profile/:accountname/followers' element={<Followers />} />
-              <Route path='/profile/followings' element={<Followers />} />
-              <Route path='/profile/:accountname/followings' element={<Followers />} />
-              <Route path='/profile/setting' element={<Setting />} />
-              <Route path='/post' element={<Post />} />
-              <Route path='/post/:id' element={<PostDetail />} />
-              <Route path='/modifypost' element={<PostModification />} />
-              <Route path='/product' element={<Product />} />
-              <Route path='/addproduct' element={<AddProduct />} />
-              <Route path='/modifyproduct' element={<ProductModification />} />
-              <Route path='/product/detail/:id' element={<ProductDetail />} />
-              <Route path='/chat' element={<Chat />} />
-              <Route path='/chat/:username' element={<ChatDetail />} />
-            </Route>
-            <Route path='*' element={<NotFound />} />
-          </>
-        </Routes>
-      </>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <>
+          <GlobalStyle />
+          {isPCScreen && <PCNavBar />}
+          <Routes>
+            <>
+              <Route path='/' element={<Landing />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/signup' element={<Signup />} />
+              <Route
+                element={
+                  <ProtectRoute>
+                    <Outlet />
+                  </ProtectRoute>
+                }
+              >
+                <Route path='/home' element={<Home />} />
+                <Route path='/search' element={<Search />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/profile/:accountname' element={<Profile />} />
+                <Route path='/profile/edit' element={<UserProfileSetting />} />
+                <Route path='/profile/followers' element={<Followers />} />
+                <Route path='/profile/:accountname/followers' element={<Followers />} />
+                <Route path='/profile/followings' element={<Followers />} />
+                <Route path='/profile/:accountname/followings' element={<Followers />} />
+                <Route path='/profile/setting' element={<Setting />} />
+                <Route path='/post' element={<Post />} />
+                <Route path='/post/:id' element={<PostDetail />} />
+                <Route path='/modifypost' element={<PostModification />} />
+                <Route path='/product' element={<Product />} />
+                <Route path='/addproduct' element={<AddProduct />} />
+                <Route path='/modifyproduct' element={<ProductModification />} />
+                <Route path='/product/detail/:id' element={<ProductDetail />} />
+                <Route path='/chat' element={<Chat />} />
+                <Route path='/chat/:username' element={<ChatDetail />} />
+              </Route>
+              <Route path='*' element={<NotFound />} />
+            </>
+          </Routes>
+        </>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 export default App;
