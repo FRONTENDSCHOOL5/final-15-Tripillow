34b2897f354b1476baa1e13c7290cf9dc4b3d388
haron <haronxlee@gmail.com>
2023-08-12 01:07:42 +0900

    Feat: 채팅 PC버전 디자인 및 PC버전 기능 추가 (#252) - PC버전시에 chatDetail로 이동안함 - PC버전시에 chat에 chatDetail 바로 띄워주기 - location으로 값을 받아오기에 isPCScreen 감지해서 state 값 계속 보내주기 - 많은 랜더링이 일어나는 문제는 있을 수 있음, 점검 필요

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import styled, { css } from 'styled-components';
 import accountName from '../../Recoil/accountName/accountName';
 import FollowingListAPI from '../../Utils/FollowingListAPI';
-import { Layout } from '../../Styles/Layout';
+import { LayoutStyle } from '../../Styles/Layout';
 import BasicHeader from '../../Components/common/Header/BasicHeader';
 import Navbar from '../../Components/common/Navbar';
 import ChatUser from './ChatUser';
 import isDesktop from '../../Recoil/isDesktop/isDesktop';
+import ChatDetail from './ChatDetail';
+import { useLocation } from 'react-router-dom';
 
 const Chat = () => {
   const isPCScreen = useRecoilValue(isDesktop);
   const accountname = useRecoilValue(accountName);
   const { fetchFollowing } = FollowingListAPI(accountname);
   const [followingData, setFollowingData] = useState([]);
+  const [isChatDetail, setIsChatDetail] = useState();
 
   useEffect(() => {
     const handleFetch = async () => {
@@ -23,27 +27,64 @@ const Chat = () => {
     handleFetch();
   }, []);
 
+  const onChatDetail = () => {
+    setIsChatDetail(true);
+  };
+
   return (
-    <Layout $isPCScreen={isPCScreen}>
-      <BasicHeader
-        btn1='설정 및 개인정보'
-        btn2='로그아웃'
-        txt='정말 로그아웃 하시겠습니까?'
-        rightbtn='확인'
-      ></BasicHeader>
-      {followingData &&
-        followingData.map((item, index) => (
-          <ChatUser
-            key={index}
-            userImg={item.image}
-            username={item.username}
-            content={item.intro}
-            account={item.accountname}
-          />
-        ))}
+    <ChatLayout $isPCScreen={isPCScreen}>
+      {!isPCScreen && (
+        <BasicHeader
+          btn1='설정 및 개인정보'
+          btn2='로그아웃'
+          txt='정말 로그아웃 하시겠습니까?'
+          rightbtn='확인'
+        ></BasicHeader>
+      )}
+      <ChatListLayout $fullSize={!isChatDetail}>
+        {followingData &&
+          followingData.map((item, index) => (
+            <ChatUser
+              key={index}
+              userImg={item.image}
+              username={item.username}
+              content={item.intro}
+              account={item.accountname}
+              onChatDetail={onChatDetail}
+            />
+          ))}
+      </ChatListLayout>
+      {isPCScreen && isChatDetail && (
+        <ChatDetailLayout>
+          <ChatDetail />
+        </ChatDetailLayout>
+      )}
       {isPCScreen || <Navbar />}
-    </Layout>
+    </ChatLayout>
   );
 };
+
+const ChatLayout = styled.main`
+  ${LayoutStyle}
+
+  ${(props) =>
+    props.$isPCScreen &&
+    css`
+      max-width: 960px;
+      display: flex;
+      padding: 0;
+      margin: 0 auto;
+    `}
+`;
+
+const ChatListLayout = styled.div`
+  flex-shrink: 0;
+  flex-basis: 40%;
+  margin: 0 auto;
+`;
+
+const ChatDetailLayout = styled.div`
+  flex-grow: 1;
+`;
 
 export default Chat;
