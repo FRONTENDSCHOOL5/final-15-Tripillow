96261d05f20d25e4b0700de3b4170f7f2bffbd19
haron <haronxlee@gmail.com>
2023-08-21 18:05:00 +0900

    Feat: Signup PC버전 (#266) Signup Login과 동일하게 컴포넌트 분리 및 재사용 PCLogin => PCSign으로 바꿔주고 /sign시에 조건부 렌더링 레이아웃 재사용 회원가입 첫페이지에서 form태그 button태그에 동일 submit 함수제거 회원가입 첫페이지에서 entert시에도 다음 컴포넌트 렌더링 할 수 있게 type변경 form 태그에 action이 Get으로 되어있기에 아이디 비번이 주소경로에 나타남, action post로 변경 회원가입 프로필 설정에서 이미지 업로드 안되는 에러 수정

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,22 +1,22 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import useLogin from '../Hooks/Sign/useLogin';
 import isDesktop from '../Recoil/isDesktop/isDesktop';
-import PCLogin from '../Components/Sign/PCLogin';
+import isLogin from '../Recoil/isLogin/isLogin';
+import PCSign from '../Components/Sign/PCSign.jsx';
 import LoginForm from '../Components/Sign/LoginForm';
 
 const Login = () => {
   const navigate = useNavigate();
   const isPCScreen = useRecoilValue(isDesktop);
-  const { isLoginState } = useLogin();
+  const isLoginState = useRecoilValue(isLogin);
 
   useEffect(() => {
     if (isLoginState) navigate('/home');
     //eslint-disable-next-line
   }, []);
 
-  return isPCScreen ? <PCLogin /> : <LoginForm />;
+  return isPCScreen ? <PCSign /> : <LoginForm />;
 };
 
 export default Login;
