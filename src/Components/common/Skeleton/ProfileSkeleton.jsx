30a00d0161fb25713f1b69a724c8aff4968360bd
haron <haronxlee@gmail.com>
2023-08-26 20:37:13 +0900

    Refactor: ProfileSkeleton 컴포넌트 분리 (#276)

---
+++ src/Components/common/Skeleton/ProfileSkeleton.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import SkeletonItem from '../../../Styles/SkeletonItem';
+
+const ProfileSkeleton = ({ userAccountname }) => {
+  return (
+    <ProfileSkeletonLayout>
+      <ProfileSkeletonItem mb='6px' />
+      <ProfileTextSkeleton mb='6px' />
+      <ProfileTextSkeleton mb='16px' />
+      <ProfileTextSkeleton mb='24px' />
+      {!userAccountname ? (
+        <>
+          <ProfileButtonSkeleton />
+          <ProfileButtonSkeleton />
+        </>
+      ) : (
+        <ProfileButtonSkeleton />
+      )}
+    </ProfileSkeletonLayout>
+  );
+};
+
+const ProfileSkeletonLayout = styled.div`
+  margin: 0 auto;
+  padding: 30px 0 26px;
+  text-align: center;
+  background-color: #fff;
+`;
+
+const ProfileSkeletonItem = styled(SkeletonItem)`
+  margin: 0 auto;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  margin-bottom: 16px;
+`;
+
+const ProfileTextSkeleton = styled(SkeletonItem)`
+  margin: 0 auto;
+  width: 120px;
+  height: 12px;
+  border-radius: 20px;
+  margin-bottom: ${(props) => props.mb || '16px'};
+`;
+
+const ProfileButtonSkeleton = styled(SkeletonItem)`
+  display: inline-block;
+  margin: 0 auto;
+  width: 100px;
+  height: 36px;
+  border-radius: 44px;
+`;
+
+export default ProfileSkeleton;
