4e358d029e78de033d872f3f7d1c6605581c344e
haron <haronxlee@gmail.com>
2023-09-11 02:01:22 +0900

    Refactor: Button aria-label 추가 (#304)

--- src/Components/Home/MyPillowings.jsx
+++ src/Components/Home/MyPillowings.jsx
@@ -6,7 +6,6 @@ import FollowUser from 'Components/common/FollowUser';
 import UseFollowing from 'Hooks/useFollowing';
 
 const MyPillowings = (props) => {
-  console.log('mypillowings');
   const { followingData } = UseFollowing();
   const { getUserData } = MyInfoAPI();
   const [user, setUser] = useState();
