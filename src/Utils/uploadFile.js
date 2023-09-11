ef5bef7568e63d00ea0c34df2215eaf59e7dac3d
haron <haronxlee@gmail.com>
2023-09-11 22:36:51 +0900

    Feat: 이미지 압축,업로드 함수 util 함수로 분리 (#331)

--- src/Utils/uploadFile.js
+++ src/Utils/uploadFile.js
@@ -1,21 +1,66 @@
 import URL from 'Api/URL';
 import ImageUploadAPI from 'Api/Upload/ImageUploadAPI';
+import imageCompression from 'browser-image-compression';
 
-export const uploadFile = async (e, imageSetter, targetObject, targetSetter) => {
+const handleDataForm = async (dataURL) => {
+  try {
+    const byteString = atob(dataURL.split(',')[1]);
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    const blob = new Blob([ia], {
+      type: 'image/jpeg',
+    });
+
+    const file = new File([blob], 'image.jpg');
+
+    const data = await ImageUploadAPI(file);
+
+    if (data) {
+      const imageUrl = data.filename;
+
+      return URL + '/' + imageUrl;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getBase64Data = async (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+};
+
+export const uploadFile = async (e, onUploadSuccess) => {
   const file = e.target.files[0];
-  const res = await ImageUploadAPI(file);
-  const imageUrl = URL + '/' + res.filename;
+  if (!file) return;
 
-  imageSetter(imageUrl);
+  const options = {
+    maxSizeMB: 0.9,
+    maxWidthOrHeight: 490,
+    useWebWorker: true,
+  };
 
-  if (targetObject && targetSetter) {
-    const updatedObject = {
-      ...targetObject,
-      user: {
-        ...targetObject.user,
-        image: imageUrl,
-      },
-    };
-    targetSetter(updatedObject);
+  try {
+    // 이미지 압축
+    const compressedFile = await imageCompression(file, options);
+
+    // base64 데이터로 변환
+    const base64data = await getBase64Data(compressedFile);
+
+    // 데이터 폼 처리 및 업로드
+    const imageUrl = await handleDataForm(base64data);
+
+    if (imageUrl) onUploadSuccess(imageUrl);
+  } catch (error) {
+    console.error(error);
   }
 };
